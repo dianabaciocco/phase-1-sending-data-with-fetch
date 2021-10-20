9042ac0c6fd722c566daab4441749b854d2c7d7d
Diana <dianabacioccoart@gmail.com>
2021-10-19 22:21:32 -0700

    commit tree 33ed2600507bb160a1c09ca095ac8610ea3bf8a8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+const submitData= function(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Accept":"application/json",
+    },
+    body:JSON.stringify({
+      "name":userName,
+      "email":userEmail
+    })
+  }).then(response=> response.json())
+  .then(data=>renderId(data))
+  .catch((error)=> {
+    const body=document.createElement("body")
+    body.innerHTML=error;
+    document.body.append(body)
+  })
+  }
+  const renderId=data=> {
+    const body= document.createElement("body")
+    body.innerHTML=data.id;
+    document.body.append(body)
+}
+
